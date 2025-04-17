3609e5051742567e179d8f7182b16b18ab980657
Coco Sun <szhcoco@gmail.com>
2025-04-17 16:09:47 -0700

    complete step 1 through 4

--- global.js
+++ global.js
@@ -14,12 +14,14 @@ function $$(selector, context = document) {
 //   currentLink.classList.add(('current'));
 // }
 
+
+
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
-  { url: 'profile/', title: 'Profile' },
+  { url: 'https://github.com/szhcoco', title: 'Profile' },
 ]
 
 let nav = document.createElement('nav');
@@ -29,5 +31,52 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-  nav.insertAdjacentElement('beforeend', `<a href="${url}">${title}</a>`);
+  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  
+  : "/portfolio/"; 
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (a.host != location.host) {
+    a.target="_blank";
+  }
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value="Automatic">Automatic</option>
+      <option value="Light">Light</option>
+      <option value="Dark">Dark</option>
+		</select>
+	</label>`,
+);
+
+const select = document.querySelector('select');
+
+if ("colorScheme" in localStorage) {
+  const color = localStorage.colorScheme;
+  document.documentElement.style.setProperty('color-scheme', color);
+  select.value = color;
+}
+
+select.addEventListener('input', function (event) {
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+
+
+       
