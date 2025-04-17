1b5317078537df6ca483bd16fb76c380004d7de8
Coco Sun <szhcoco@gmail.com>
2025-04-17 14:38:53 -0700

    create nav bar for pages using js

--- global.js
+++ global.js
@@ -4,3 +4,30 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+// navlinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// if (currentLink) {
+//   currentLink.classList.add(('current'));
+// }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'profile/', title: 'Profile' },
+]
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  nav.insertAdjacentElement('beforeend', `<a href="${url}">${title}</a>`);
+}
